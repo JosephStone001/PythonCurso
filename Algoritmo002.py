46e99ec7a856663b7b71a6b4e104235a5b04bcc8
JoseRocha <rocha_the_designer@hotmail.com>
2024-12-02 21:43:21 -0300

    update

---
+++ Algoritmo002.py
@@ -0,0 +1,8 @@
+Jose = {"Documentos":[576992579, 4769925779,22261160], 
+        "Caracteristicas":[1,68, "Escuros","Catanhos",73], 
+        "Hobbys":["jiu-jitsu", "jogos", "filmes", "Desenhos", "series"], 
+        "TipoPessoa":"Pessoa Fisica"}
+
+Jose.update("Valor")
+
+print(Jose)
